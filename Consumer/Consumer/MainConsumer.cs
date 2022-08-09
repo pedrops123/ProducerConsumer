f23910816ddb8efafacf3af3bcbee2edfdbfaae3
pedro furlan <pedro.furlan@globalsys.com.br>
2022-08-09 16:47:22 -0300

    Finalizacao Consumer Producer

---
+++ Consumer/Consumer/MainConsumer.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public sealed class MainConsumer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queue = channel.QueueDeclare
+            (
+               "fila_teste",
+                false,
+                false,
+                false,
+                null
+            );
+
+            Console.WriteLine($"Coletando do Queue : { queue.QueueName }");
+
+            Console.WriteLine($"Total de mensagens : { queue.MessageCount }");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"Mensagem recebida : { message }");
+            };
+
+            channel.BasicConsume(
+                 "fila_teste",
+                  true,
+                  consumer);
+        }
+    }
+}
---
+++ Producer/Producer/MainProducer.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Producer
+{
+    public sealed class MainProducer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.ConfirmSelect();
+
+                var queue = channel.QueueDeclare
+                 (
+                        queue: "fila_teste",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                     );
+
+                Console.WriteLine($"Iniciando no Queue { queue.QueueName }");
+
+                string message = "Teste de mensagem corpo.";
+
+                var parsedBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "fila_teste",
+                    basicProperties: null,  
+                    body: parsedBody);
+
+                channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 1, 50));
+
+                if (queue.MessageCount > 0)
+                {
+                    Console.WriteLine($"* Mensagem Enviada com Sucesso ! Mensagem : { message }");
+                }
+                else
+                {
+                    Console.WriteLine("Nenhuma mensagem enviada !");
+                }
+            }
+        }
+    }
+}
